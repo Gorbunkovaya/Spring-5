f072dedcce77717fbc00ab0db5eb0603229de26c
Gorbunkovaya <dgorbunkovaya@gmail.com>
2021-07-28 17:04:07 +0300

    new

--- 2.2.2. spring_mvc/src/main/java/web/controller/CarController.java
+++ 2.2.2. spring_mvc/src/main/java/web/controller/CarController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
+import web.service.CarService;
 import web.service.CarServiceImpl;
 
 import java.util.List;
@@ -14,7 +15,7 @@ public class CarController {
 
     @GetMapping(value = "/cars")
     public String printCars(@RequestParam(value = "count", required = false) Integer count, ModelMap model) {
-        CarServiceImpl cs = new CarServiceImpl();
+        CarService cs = new CarServiceImpl();
         List <Car> listCar = cs.createCarsByCount(count);
         model.addAttribute("car", listCar);
         return "cars";
